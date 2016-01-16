43bb250b78dca6f2d14a19a89a680c857c69d486
Adrian Webb <adrian.webb@coraltech.net>
2016-01-15 22:29:55 -0500

    Editor updates.

--- AptanaStudio3.sh
+++ AptanaStudio3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export UBUNTU_MENUPROXY=0
-./AptanaStudio3
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+"$SCRIPT_DIR"/AptanaStudio3
